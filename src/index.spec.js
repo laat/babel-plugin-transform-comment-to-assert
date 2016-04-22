e6007b4994a0a96f1d4a0c814e5aa95973c1443e
Sigurd Fosseng <sigurd@fosseng.net>
2016-04-22 21:18:25 +0200

    tape

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,82 +3,70 @@ import commentVisitor from './index.js';
 import traverse from 'babel-traverse';
 import generate from 'babel-generator';
 import { transform } from 'babel-core';
-import { expect } from 'chai';
+import test from 'tape';
 
-function testGeneration(code, expectedCode) {
-  const { ast } = transform(code);
-  traverse(ast, commentVisitor().visitor);
-  const transformedCode = generate(ast, {}, code).code;
-  expect(transformedCode.trim()).to.equal(expectedCode.trim());
-}
+test('should add string asserts', (t) => {
+  const testGeneration = (code, expectedCode, message) => {
+    const { ast } = transform(code);
+    traverse(ast, commentVisitor().visitor);
+    const transformedCode = generate(ast, {}, code).code;
+    t.equal(transformedCode.trim(), expectedCode.trim(), message);
+  };
 
-describe('Babel comment assert equal', () => {
-  it('should add string asserts', () => {
-    testGeneration(`
+  testGeneration(`
 a = "foobar"
 a //=> "foobar"
 `, `
 a = "foobar";
 assert.deepEqual(a, "foobar");
-`);
-  });
+`, 'add string asserts');
 
-  it('should add number asserts', () => {
-    testGeneration(`
+  testGeneration(`
 a = 1;
 a //=> 1
 `, `
 a = 1;
 assert.deepEqual(a, 1);
-`);
-  });
+`, 'should add number asserts');
 
-  it('should add Array asserts', () => {
-    testGeneration(`
+  testGeneration(`
 a = [1, 2, 3];
 a //=> [1, 2, 3]
 `, `
 a = [1, 2, 3];
 assert.deepEqual(a, [1, 2, 3]);
-`);
-  });
+`, 'should add Array asserts');
 
-  it('should add Object asserts', () => {
-    testGeneration(`
+  testGeneration(`
 a = { "a": 1 }
 a //=> {a: 1}
 `, `
 a = { "a": 1 };
 assert.deepEqual(a, { a: 1 });
-`);
-  });
+`, 'should add Object asserts');
 
-  it('should add console.log asserts', () => {
-    testGeneration(`
+  testGeneration(`
 a = { "a": 1 }
 console.log(a) //=> {a: 1}
 `, `
 a = { "a": 1 };
 console.log(a);assert.deepEqual(a, { a: 1 });
-`);
-  });
-  it('should add functioncall asserts', () => {
-    testGeneration(`
+`, 'should add console.log asserts');
+
+  testGeneration(`
 a = { "a": 1 }
 foobar(a) //=> {a: 1}
 `, `
 a = { "a": 1 };
 assert.deepEqual(foobar(a), { a: 1 });
-`);
-  });
+`, 'should add functioncall asserts');
 
-  it('should add Object property asserts', () => {
-    testGeneration(`
+  testGeneration(`
 a = { "foo": 1 }
 a.foo //=> 1
 `, `
 a = { "foo": 1 };
 assert.deepEqual(a.foo, 1);
-`);
-  });
+`, 'should add Object property asserts');
+  t.end();
 });
