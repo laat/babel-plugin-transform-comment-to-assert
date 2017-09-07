54f6ba169dc03e8d14e3cf50fab93ab4d78609bf
Sigurd Fosseng <sigurd@fosseng.net>
2017-09-07 21:20:22 +0200

    upgrade eslint + add prettier

--- src/index.js
+++ src/index.js
@@ -4,13 +4,8 @@ const arrowRegex = /^\s?(=>|→|throws)/;
 export default function visitor({ types: t, transform }) {
   const assertExpression = (actual, expected, loc) => {
     const exp = t.callExpression(
-      t.memberExpression(
-        t.identifier('assert'),
-        t.identifier('deepEqual'),
-      ), [
-        actual,
-        expected,
-      ]
+      t.memberExpression(t.identifier('assert'), t.identifier('deepEqual')),
+      [actual, expected]
     );
     exp.loc = loc;
     return exp;
@@ -18,13 +13,8 @@ export default function visitor({ types: t, transform }) {
 
   const throwsExpression = (body, arg, loc) => {
     const exp = t.callExpression(
-      t.memberExpression(
-        t.identifier('assert'),
-        t.identifier('throws'),
-      ), [
-        t.arrowFunctionExpression([], body),
-        arg,
-      ]
+      t.memberExpression(t.identifier('assert'), t.identifier('throws')),
+      [t.arrowFunctionExpression([], body), arg]
     );
     exp.loc = loc;
     return exp;
@@ -34,9 +24,11 @@ export default function visitor({ types: t, transform }) {
     visitor: {
       ExpressionStatement(path) {
         const comments = path.node.trailingComments;
-        if (comments &&
-            comments.length > 0 &&
-            comments[0].value.match(arrowRegex)) {
+        if (
+          comments &&
+          comments.length > 0 &&
+          comments[0].value.match(arrowRegex)
+        ) {
           const matches = comments[0].value.match(arrowRegex);
           const throws = matches[1] === 'throws';
 
@@ -44,17 +36,29 @@ export default function visitor({ types: t, transform }) {
 
           const commentLoc = comments[0].loc;
           const rawComment = comments[0].value.replace(arrowRegex, '').trim();
-          const comment = transform(`() => (${rawComment})`).ast.program.body[0].expression.body;
+          const comment = transform(`() => (${rawComment})`).ast.program.body[0]
+            .expression.body;
 
-          if (t.isCallExpression(child) &&
-                  child.callee && child.callee.object &&
-                  child.callee.object.name === 'console') {
+          if (
+            t.isCallExpression(child) &&
+            child.callee &&
+            child.callee.object &&
+            child.callee.object.name === 'console'
+          ) {
             path.node.trailingComments = comments.splice(1); // eslint-disable-line
             const code = child.arguments[0];
             if (throws) {
-              path.insertAfter(t.expressionStatement(throwsExpression(code, comment, commentLoc)));
+              path.insertAfter(
+                t.expressionStatement(
+                  throwsExpression(code, comment, commentLoc)
+                )
+              );
             } else {
-              path.insertAfter(t.expressionStatement(assertExpression(code, comment, commentLoc)));
+              path.insertAfter(
+                t.expressionStatement(
+                  assertExpression(code, comment, commentLoc)
+                )
+              );
             }
           } else {
             path.node.trailingComments = comments.splice(1); // eslint-disable-line
--- src/index.spec.js
+++ src/index.spec.js
@@ -3,102 +3,149 @@ import assert from 'assert-simple-tap';
 import commentVisitor from './index';
 
 const testGeneration = (code, expectedCode, message) => {
-  const actual = babel.transform(code, { babelrc: false, plugins: [commentVisitor] }).code;
+  const actual = babel.transform(code, {
+    babelrc: false,
+    plugins: [commentVisitor],
+  }).code;
   assert.equal(actual.trim(), expectedCode.trim(), message);
 };
 
-testGeneration(`
+testGeneration(
+  `
 a = "foobar"
 a //=> "foobar"
-`, `
+`,
+  `
 a = "foobar";
 assert.deepEqual(a, "foobar");
-`, 'add string asserts');
+`,
+  'add string asserts'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = 1;
 a //=> 1
-`, `
+`,
+  `
 a = 1;
 assert.deepEqual(a, 1);
-`, 'should add number asserts');
+`,
+  'should add number asserts'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = [1, 2, 3];
 a //=> [1, 2, 3]
-`, `
+`,
+  `
 a = [1, 2, 3];
 assert.deepEqual(a, [1, 2, 3]);
-`, 'should add Array asserts');
+`,
+  'should add Array asserts'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = { "a": 1 }
 a //=> {a: 1}
-`, `
+`,
+  `
 a = { "a": 1 };
 assert.deepEqual(a, { a: 1 });
-`, 'should add Object asserts');
+`,
+  'should add Object asserts'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = { "a": 1 }
 console.log(a); //=> {a: 1}
-`, `
+`,
+  `
 a = { "a": 1 };
 console.log(a);assert.deepEqual(a, { a: 1 });
-`, 'should add console.log asserts');
+`,
+  'should add console.log asserts'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = { "a": 1 }
 foobar(a) //=> {a: 1}
-`, `
+`,
+  `
 a = { "a": 1 };
 assert.deepEqual(foobar(a), { a: 1 });
-`, 'should add functioncall asserts');
+`,
+  'should add functioncall asserts'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = { "foo": 1 }
 a.foo //=> 1
-`, `
+`,
+  `
 a = { "foo": 1 };
 assert.deepEqual(a.foo, 1);
-`, 'should add Object property asserts');
+`,
+  'should add Object property asserts'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = "foobar"
 a // => "foobar"
-`, `
+`,
+  `
 a = "foobar";
 assert.deepEqual(a, "foobar");
-`, 'add string asserts with space before arrow');
+`,
+  'add string asserts with space before arrow'
+);
 
-testGeneration(`
+testGeneration(
+  `
 a = "foobar"
 a // → "foobar"
-`, `
+`,
+  `
 a = "foobar";
 assert.deepEqual(a, "foobar");
-`, 'add string asserts with space utf8 arrow');
+`,
+  'add string asserts with space utf8 arrow'
+);
 
-testGeneration(`
+testGeneration(
+  `
 const a = () => {
   throw new Error('fail');
 };
 a() // throws /fail/
-`, `
+`,
+  `
 const a = () => {
   throw new Error('fail');
 };
 assert.throws(() => a(), /fail/);
-`, 'throws expression');
+`,
+  'throws expression'
+);
 
-testGeneration(`
+testGeneration(
+  `
 const a = () => {
   throw new Error('fail');
 };
 a() // throws Error
-`, `
+`,
+  `
 const a = () => {
   throw new Error('fail');
 };
 assert.throws(() => a(), Error);
-`, 'throws expression');
+`,
+  'throws expression'
+);
